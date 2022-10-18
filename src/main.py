1f26587dbe2c8b9395a35dd38323da8e8e2fe0e7
Mahmudur <mahmudhera93@gmail.com>
2022-10-18 19:32:02 -0400

    added code for opacity

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ import argparse
 import pandas as pd
 import graphviz
 
+max_opacity = 30
+max_width = 10
+max_length_in_inches = 7
+
 def parse_args(): # pragma: no cover
     parser = argparse.ArgumentParser(description="This script will plot the given two graphs. Assumed that both graphs have the same edges, only differing in their lengths of these branches.  Assumed files are in tsv format, three columns, first two are node names, the third is the branch length",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -10,7 +14,7 @@ def parse_args(): # pragma: no cover
     args = parser.parse_args()
     return args
 
-last = -500
+last = 0
 dic = {}
 def get_encoding(vertex_name):
     global last
@@ -21,6 +25,14 @@ def get_encoding(vertex_name):
         dic[vertex_name] = str(last)
         last = last + 1
         return dic[vertex_name]
+
+def get_hex_from_intensity(intensity):
+    in_numbers = int(max_opacity * intensity) + 10
+    ret_str = "{x:X}".format(x=in_numbers)
+    if len(ret_str) == 1:
+        return '0' + ret_str
+    else:
+        return ret_str
 
 def main(): # pragma: no cover
     args = parse_args()
@@ -38,13 +50,14 @@ def main(): # pragma: no cover
     sum_of_lengths = [ x+y for (x,y) in list(zip(lengths_in_graph1, lengths_in_graph2)) ]
     difference_of_lengths = [ abs(x-y) for (x,y) in list(zip(lengths_in_graph1, lengths_in_graph2)) ]
 
-    color_intensity = [ 1.0*x/y for (x,y) in list( zip(difference_of_lengths, sum_of_lengths) ) ]
+    intensity_list = [ 1.0*x/y for (x,y) in list( zip(difference_of_lengths, sum_of_lengths) ) ]
 
     # graphs
     g = graphviz.Graph('G', filename='hello.gv', engine='sfdp', format='svg', node_attr={'width' : '0.02', 'height':'0.02', 'fixedsize':'true', 'label':''})
-    for (v1, v2, color, length) in list( zip(vertex_1_list, vertex_2_list, color_intensity, lengths_in_graph1) ):
-        pen_width = str( 7.0 * color )
-        g.edge( get_encoding(v1.strip()) , get_encoding(v2.strip()), **{'color':'red', 'penwidth':pen_width, 'len':str(5*length)} )
+    for (v1, v2, intensity, length) in list( zip(vertex_1_list, vertex_2_list, intensity_list, lengths_in_graph1) ):
+        pen_width = str( max_width * intensity )
+        opacity = get_hex_from_intensity(intensity)
+        g.edge( get_encoding(v1.strip()) , get_encoding(v2.strip()), **{'color':'#0000ff' + opacity, 'penwidth':pen_width, 'len':str(max_length_in_inches*length)} )
     g.view()
 
 if __name__ == '__main__':
